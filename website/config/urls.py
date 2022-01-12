edb87cea69ea786e9518e3afbec37d24c22e02bc
O Moo <omoog10101@gmai.com>
2022-01-11 22:48:53 -0700

    added picture folder

--- website/config/urls.py
+++ website/config/urls.py
@@ -19,5 +19,5 @@ from django.urls import path
 
 # wesite urls
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', admin.site.urls),
 ]
--- website/store/models.py
+++ website/store/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
+
 # Create your models here.
+
+
+# class Store (models.Model):
+#     name = models.CharField(max_length=100)
+#     description = models.TextField()
+#     cost = models.TextField(max_length=300, default="")
+#     quanity = models.TextField(max_length=300, default="")
+#     image = models.CharField(max_length=200)
+
+#     def __str__(self):
+#         return f"{self.name}, {self.description}"
+
+#     def get_absolute_url(self):
+#         pass
