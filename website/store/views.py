07dce334f88a6716ffd4b016ee773dca3f8cdf73
O Moo <omoog10101@gmai.com>
2022-01-14 12:43:26 -0700

    basic home page created and urls are all hooked up

--- website/store/views.py
+++ website/store/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
 
+from django.shortcuts import render
+from django.views.generic import TemplateView
+from .models import Store
 # Create your views here.
+
+
+class HomePage(TemplateView):
+    model = Store
+    template_name = "home.html"
+
+
+class AdminPage(TemplateView):
+    mode = Store
+    template_name = "adminpage.html"
