b2fbc53e5d5a4191b8efbae4cbc01d50f9ebd4f6
O Moo <omoog10101@gmai.com>
2022-01-24 19:07:30 -0700

    redesign the add item page

--- website/store/models.py
+++ website/store/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 # Create your models here.
 class Product(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=300)
     cost = models.CharField(max_length=300, default="")
     # description = models.TextField(max_length=300, default="")
     quantity = models.CharField(max_length=300, default="")
